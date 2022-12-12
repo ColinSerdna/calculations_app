b3f83b54e46d22af3b8254ea1a6ea6b4844a1658
Colin <rca.serdn@gmail.com>
2022-12-12 00:14:24 -0600

    Initial version calculations app

---
+++ src/main/java/mx/com/rca/app/CalculationsAppApplication.java
@@ -0,0 +1,13 @@
+package mx.com.rca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/rca/app/CalculationsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.rca.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
